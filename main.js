1a8f50161ccc4452e7846d0ba570df327d6f5622
AndriiGriMM <webdevgrimm@gmail.com>
2022-07-13 16:50:51 +0300

    1 try

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-const requestURL = 'http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19'
-function request(method, url) {
-  return fetch(url).then(response => {
-    return response.json()
-  })
+function Posts(params) {
+  params.button.addEventListener("click", () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.input.value}`)
+      .then(function (response) {
+        if (response.status === 200)
+          getPosts()
+        resset()
+      })
+  });
+
+  function getPosts() {
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${params.input.value}`)
+      .then(response => response.json())
+      .then(json => renderPosts(json))
+  }
+
+  function renderPosts(posts) {
+    for (var i = 0; i < posts.length; i++) {
+      let div = document.createElement("h2");
+      let body = document.createElement("p");
+      div.innerHTML = posts[i].name
+      body.innerHTML = posts[i].body
+      params.coments.appendChild(div)
+      params.coments.appendChild(body)
+    }
+  }
+
+  function resset() {
+    params.coments.innerHTML = '';
+  }
 }
-request('GET', requestURL)
-  .then(data => {
-    document.querySelector('.city').innerHTML = 'City' + ' : ' + JSON.stringify(data.name)
-    document.querySelector('.temp').innerHTML = 'Temperature' + ' : ' + JSON.stringify(data.main.temp)
-    document.querySelector('.pressure').innerHTML = 'Pressure' + ' : ' + JSON.stringify(data.main.pressure)
-    document.querySelector('.description').innerHTML = 'Description' + ' : ' + JSON.stringify(data.weather[0].description)
-    document.querySelector('.humidity').innerHTML = 'Humidity' + ' : ' + JSON.stringify(data.main.humidity)
-    document.querySelector('.speed').innerHTML = 'Speed' + ' : ' + JSON.stringify(data.wind.speed)
-    document.querySelector('.deg').innerHTML = 'Wind deg' + ' : ' + JSON.stringify(data.wind.deg)
-    document.querySelector('.img-icon').setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png `)
-  })
+
+new Posts({
+  input: document.querySelector('.js-input'),
+  coments: document.querySelector('.coments'),
+  button: document.querySelector('.btn'),
+})
